49ca795f59d345118288955c45dbf2ab80bd38cf
hleesa <salee2@student.42seoul.kr>
2024-08-08 21:12:46 +0900

    Time: 80 ms (5.04%), Space: 13.8 MB (23.17%) - LeetHub

---
+++ 0921-spiral-matrix-iii/0921-spiral-matrix-iii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> visited;
+        Move cur(rStart, cStart, 0, 1, 0);
+        cur.move(visited, rows, cols);
+        return visited;
+    }
+
+private:
+    class Move {
+        private:
+
+        int row;
+        int col;
+        int dir;
+        int distance;
+        int count;
+        const int deltaPos[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+
+        bool outOfMatrix(int rows, int cols){
+            if (row < 0 || rows <= row ) return true;
+            else if (col < 0 || cols <= col ) return true;
+            return false;
+        }
+
+        vector<int> getPos(){
+            return vector<int>({row,col});
+        }
+
+        public:
+
+        Move(int row, int col, int dir, int distance, int count) : row(row), col(col),
+        dir(dir), distance(distance), count(count){}
+
+        void move(vector<vector<int>>& visited, int rows, int cols){
+            while(true){
+                for(int i =0; i < distance; ++i){
+                    if (!outOfMatrix(rows, cols)){
+                        visited.push_back(getPos());
+                        if(visited.size() >= rows * cols){
+                            return;
+                        }
+                    }
+                    cout << row << ", " << col << "\n";
+                    row += deltaPos[dir][0];
+                    col += deltaPos[dir][1];
+                }
+                ++count;
+                if(count % 2 == 0){
+                    ++distance;
+                }
+                dir = (dir + 1 ) % 4;
+            }
+        }
+    };
+};
