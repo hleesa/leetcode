36499cf3e8b1003db8a55e2749c6f97c42d8dc46
hleesa <salee2@student.42seoul.kr>
2024-09-03 20:25:38 +0900

    Time: 4 ms (39.15%), Space: 8 MB (77.66%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int getLucky(string s, int k) {
-        string str = convert(s);
-        for(int i = 0; i < k; ++i){
-            str = transform(str);
+        int ans = 0;
+        for(auto c : s){
+            int n = c - 'a' + 1;
+            while(n > 0){
+                ans += n % 10;
+                n /= 10;
+            }
         }
-        return stoi(str);
-    }
-private:
-    string convert(string s){
-        string ret;
-        for(const char c : s){
-            ret += to_string(c - 'a' + 1);
+        for(int i = 1; i < k; ++i){
+            int digits = 0;
+            while(ans > 0){
+                digits += ans % 10;
+                ans /= 10;
+            }
+            ans = digits;
         }
-        return ret;
-    }
-    string transform(string s){
-        int ret = 0;
-        for(const char c : s){
-            ret += c - '0';
-        }
-        return to_string(ret);
+        return ans;
     }
 };
