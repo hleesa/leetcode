a26e4100c1e305b81523747b55f74306078650cf
hleesa <salee2@student.42seoul.kr>
2024-09-11 20:50:42 +0900

    Time: 0 ms (100%), Space: 7.6 MB (81.24%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int minBitFlips(int start, int goal) {        
-        int ret = 0;
-        int num = start ^ goal;
-        for(int i = 0; (1 << i) <= num; ++i){
-            if((num & (1 << i))){
-                ++ret;
-            }
-        }
-        return ret;
+    int minBitFlips(int start, int goal) {
+        return __builtin_popcount(start^goal);
     }
 };
