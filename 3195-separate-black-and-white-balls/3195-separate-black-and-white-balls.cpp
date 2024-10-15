fac716fdc385a1e1f1b40c85316e983e216a50f7
hleesa <salee2@student.42seoul.kr>
2024-10-15 21:36:41 +0900

    Time: 45 ms (5.89%), Space: 25 MB (6.32%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        const int n = s.length(); 
+        deque<int> zeroIndices, oneIndices;
+        for(int i = 0; i < n; ++i){
+            if (s[i] == '0'){
+                zeroIndices.push_back(i);
+            }
+            else{
+                oneIndices.push_back(i);
+            }
+        }
+
+        long long steps = 0;
+        while(!oneIndices.empty() && !zeroIndices.empty() && oneIndices.front() < zeroIndices.back()){
+            steps += zeroIndices.back() - oneIndices.front();
+            oneIndices.pop_front();
+            zeroIndices.pop_back();
+        }
+
+        return steps;
+    }
+};
+
+/*
+
+101
+
+1.
+
+100
+
+11010
+0 1
+01110
+
+11100
+11100
+11001
+*/
