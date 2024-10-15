838d383522fb9386f1c42c91ce7f37fd44c6d6d9
hleesa <salee2@student.42seoul.kr>
2024-10-15 21:44:11 +0900

    Time: 31 ms (73.47%), Space: 15.3 MB (95.58%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     long long minimumSteps(string s) {
-        const int n = s.length(); 
-        deque<int> zeroIndices, oneIndices;
-        for(int i = 0; i < n; ++i){
-            if (s[i] == '0'){
-                zeroIndices.push_back(i);
+        long long steps = 0, ones = 0;
+        for(auto c : s){
+            if (c == '1'){
+                ++ones;
             }
             else{
-                oneIndices.push_back(i);
+                steps += ones;
             }
         }
-
-        long long steps = 0;
-        while(!oneIndices.empty() && !zeroIndices.empty() && oneIndices.front() < zeroIndices.back()){
-            steps += zeroIndices.back() - oneIndices.front();
-            oneIndices.pop_front();
-            zeroIndices.pop_back();
-        }
-
+        
         return steps;
     }
 };
