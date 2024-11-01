ead3ca09374f7f0faa9a869c9da75f51e60d21c5
hleesa <salee2@student.42seoul.kr>
2024-11-01 14:51:21 +0900

    Time: 51 ms (5.19%), Space: 42.8 MB (63.26%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string fancyStr;
+        if(s.length() < 3){
+            return s;
+        }
+        string fancyStr = s.substr(0, 2);
         int counter[26] = {0};
+        ++counter[s[0]-'a'];
+        ++counter[s[1]-'a'];
 
-        for(int i = 0; i < s.length(); ++i){
+        for(int i = 2; i < s.length(); ++i){
             int inIdx = s[i] - 'a';
             ++counter[inIdx];
-            
             if(counter[inIdx] < 3){
                 fancyStr.push_back(s[i]);
             }
-            if(i >= 2){
-                --counter[s[i-2] - 'a'];
-            }
+            --counter[s[i-2] - 'a'];
         }
 
         return fancyStr;
