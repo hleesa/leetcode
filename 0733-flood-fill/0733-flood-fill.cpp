e2e6236a49447c88d3c3674007e4df3bb369304c
hleesa <salee2@student.42seoul.kr>
2024-11-13 18:57:52 +0900

    Time: 0 ms (100%), Space: 17 MB (66.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<bool>> vis(image.size(), vector<bool>(image.front().size()));
-        floodFillHelper(image, vis, sr, sc, image[sr][sc], color);
+        if(image[sr][sc] == color){
+            return image;
+        }
+        floodFillHelper(image, sr, sc, image[sr][sc], color);
         return image;
     }
+    
 private:
     const int dr[4] = {-1,1,0,0};
     const int dc[4] = {0,0,-1,1};
 
-    void floodFillHelper(vector<vector<int>>& image, vector<vector<bool>>& vis, int r, int c, int initColor, int color){
+    void floodFillHelper(vector<vector<int>>& image, int r, int c, int initColor, int color){
         if(r < 0 || r >= image.size() || c < 0 || c >= image.front().size()) return;
-        else if(vis[r][c]) return;
-        else if(image[r][c] != initColor) return;
-        vis[r][c] = true;
+        else if (image[r][c] != initColor) return;
+
         image[r][c] = color;
         for(int i = 0; i < 4; ++i){
-            floodFillHelper(image, vis, r+dr[i], c+dc[i], initColor, color);
+            floodFillHelper(image, r+dr[i], c+dc[i], initColor, color);
         }
     }
 };
