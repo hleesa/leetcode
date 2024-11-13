a7a97536c2a1eaf4bf59409e65b44125357deb30
hleesa <salee2@student.42seoul.kr>
2024-11-13 18:53:46 +0900

    Time: 1 ms (22.7%), Space: 17.3 MB (30.71%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        const int rEnd = image.size();
-        const int cEnd = image.front().size();
-        vector<vector<bool>> vis(rEnd, vector<bool>(cEnd));
-        ff(image, vis, sr, sc, image[sr][sc], color, rEnd, cEnd);
+        vector<vector<bool>> vis(image.size(), vector<bool>(image.front().size()));
+        floodFillHelper(image, vis, sr, sc, image[sr][sc], color);
         return image;
     }
 private:
     const int dr[4] = {-1,1,0,0};
     const int dc[4] = {0,0,-1,1};
 
-    void ff(vector<vector<int>>& image, vector<vector<bool>>& vis, int r, int c, int initColor, int color, int rEnd, int cEnd){
-        if(r < 0 || r >= rEnd || c < 0 || c >= cEnd) return;
+    void floodFillHelper(vector<vector<int>>& image, vector<vector<bool>>& vis, int r, int c, int initColor, int color){
+        if(r < 0 || r >= image.size() || c < 0 || c >= image.front().size()) return;
         else if(vis[r][c]) return;
         else if(image[r][c] != initColor) return;
         vis[r][c] = true;
         image[r][c] = color;
         for(int i = 0; i < 4; ++i){
-            ff(image, vis, r+dr[i], c+dc[i], initColor, color, rEnd, cEnd);
+            floodFillHelper(image, vis, r+dr[i], c+dc[i], initColor, color);
         }
     }
 };
