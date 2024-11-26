7bcb79fde4cb03d6fbe4d748e1471e82e8970327
hleesa <salee2@student.42seoul.kr>
2024-11-26 14:53:50 +0900

    Time: 4 ms (72.43%), Space: 93.5 MB (70.3%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.cpp
+++ 3189-find-champion-ii/3189-find-champion-ii.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
-        vector<int> inOrder(n);
+        vector<int> inDegree(n);
         for(const auto& edge : edges){
-            ++inOrder[edge.back()];
+            ++inDegree[edge.back()];
         }
         vector<int> ans;
         for(int i = 0; i < n; ++i){
-            if(inOrder[i] == 0){
+            if(inDegree[i] == 0){
                 ans.push_back(i);
             }
         }
