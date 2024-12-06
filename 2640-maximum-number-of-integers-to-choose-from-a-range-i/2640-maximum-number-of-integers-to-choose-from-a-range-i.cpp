aa79cd61a769c9017f4fd8062aabb8d729799ef6
hleesa <salee2@student.42seoul.kr>
2024-12-06 17:42:42 +0900

    Time: 307 ms (15.65%), Space: 185.6 MB (23.12%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -5,7 +5,7 @@ public:
         int cnt = 0, sum = 0;
         for(int i = 1; i <= n; ++i){
             if(ban.contains(i)) continue;
-            if(sum + i > maxSum) break;
+            else if(sum + i > maxSum) break;
             sum += i;
             ++cnt;
         }
