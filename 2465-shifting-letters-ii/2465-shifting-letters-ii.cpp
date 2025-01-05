5ab8b4200fa033b9f1492fc6b7c5e4ac6f8f1f59
hleesa <salee2@student.42seoul.kr>
2025-01-05 09:25:01 +0900

    Time: 4 ms (76.8%), Space: 98.7 MB (46.2%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -3,16 +3,16 @@ public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
         const int n = s.length();
         const int mod = 26;
-        vector<int> in(n+1), out(n+1);
+        vector<int> pShift(n+1);
         for(const auto& shift : shifts){
             int sign = shift.back() == 1 ? 1 : -1;
-            in[shift[0]] = (in[shift[0]] + sign + mod) % mod;
-            out[shift[1]+1] = (out[shift[1]+1] -sign + mod) % mod;
+            pShift[shift[0]] = (pShift[shift[0]] + sign + mod) % mod;
+            pShift[shift[1]+1] = (pShift[shift[1]+1] -sign + mod) % mod;
         }
         string ans;
         int cur = 0;
         for(int i = 0; i < n; ++i){
-            cur += in[i] + out[i];
+            cur += pShift[i];
             ans.push_back((s[i] - 'a' + cur + mod) %mod + 'a');
         }
         return ans;
