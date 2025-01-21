683086bb1f2cc6e95eede00f563d4ac03ff48281
hleesa <salee2@student.42seoul.kr>
2025-01-21 23:19:22 +0900

    Time: 7 ms (30.77%), Space: 75.6 MB (33.03%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
-        int n = grid[0].size();
-        vector<long long> top(n + 1, 0), bottom(n + 1, 0);
-        
-        for (int i = 0; i < n; i++) {
-            top[i + 1] = top[i] + grid[0][i];
-            bottom[i + 1] = bottom[i] + grid[1][i];
+        const int n = grid.front().size();
+        vector<long long> top(n+1), bottom(n+1);
+
+        for(int i = 0; i < n; ++i){
+            top[i+1] = top[i] + grid[0][i];
+            bottom[i+1] = bottom[i] + grid[1][i];
         }
-        
-        long long result = LLONG_MAX;
-        
-        for (int x = 0; x < n; x++) {
-            long long second_robot = max(top[n] - top[x + 1], bottom[x]); 
-            result = min(result, second_robot);
+
+        long long ans = LLONG_MAX;
+        for(int x = 0; x < n; ++x){
+            long long secondRobot = max(top[n] - top[x+1], bottom[x]);
+            ans = min(secondRobot, ans);
         }
-        
-        return result;
+
+        return ans;
     }
 };
