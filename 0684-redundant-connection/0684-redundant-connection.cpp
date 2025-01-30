b0af2e855b4654fe7bf66c2abbe89dd28b6c3ed2
hleesa <salee2@student.42seoul.kr>
2025-01-30 22:25:13 +0900

    Time: 3 ms (37.15%), Space: 12.7 MB (93.59%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,32 +1,31 @@
 class Solution {
-private:
-    vector<int> parent;
-
-    int find(int x) {
-        if (parent[x] == -1) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    bool unite(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX == rootY) return false;
-
-        parent[rootY] = rootX;
-        return true;
-    }
-
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int N = edges.size();
-        parent.assign(N, -1);
-
-        for (auto edge : edges) {
-            if (!unite(edge[0] - 1, edge[1] - 1)) {
+        const int n = edges.size();
+        parent.resize(n+2, -1);
+        for(const auto& edge : edges){
+            if (!unite(edge.front(), edge.back())){
                 return edge;
             }
         }
 
-        return {};
+        return vector<int>();
+    }
+
+private:
+    vector<int> parent;
+
+    int find(int x){
+        if (parent[x] == -1) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    bool unite(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+        if(rootX == rootY) return false;
+        
+        parent[rootY] = rootX;
+        return true;
     }
 };
