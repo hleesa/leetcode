2867300b7423e6bacf177eae7bcf184ccbecaca1
hleesa <salee2@student.42seoul.kr>
2025-02-06 21:58:20 +0900

    Time: 908 ms (6.58%), Space: 99.5 MB (6.27%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        map<int,int> mulToNum;
+        const int n = nums.size();
+        for(int i = 0; i + 1< n; ++i){
+            for(int j = i + 1; j < n; ++j){
+                mulToNum[nums[i]* nums[j]]++;
+            }
+        }
+
+        int numTutples = 0;
+        for(auto it : mulToNum){
+            if(it.second > 1){
+                numTutples +=  2 * (it.second) * 2 * (it.second-1);
+            }
+        }
+
+        return numTutples;
+    }
+};
+
