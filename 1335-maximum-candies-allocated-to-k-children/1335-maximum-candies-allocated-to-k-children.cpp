d4985e775325b3c4957cb6fbdd12a98dc8da14ef
hleesa <salee2@student.42seoul.kr>
2025-03-14 19:49:30 +0900

    Time: 44 ms (20.55%), Space: 88.2 MB (73.33%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -3,14 +3,14 @@ public:
     int maximumCandies(vector<int>& candies, long long k) {
         int ans = 0;
         long long l = 1, r = INT_MAX;
-        while(l < r){
+        while(l <= r){
             long long mid = l + (r-l) / 2;
             if(calloc(candies, mid, k)){
                 ans = max((int)mid, ans);
                 l = mid + 1;
             }
             else{
-                r = mid;
+                r = mid - 1;
             }
         }
         return ans;
