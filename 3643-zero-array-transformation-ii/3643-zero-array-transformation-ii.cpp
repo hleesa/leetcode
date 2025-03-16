03e8ac8367793bff232bc3491b2f87f59b341024
hleesa <salee2@student.42seoul.kr>
2025-03-16 09:58:20 +0900

    Time: 963 ms (5.01%), Space: 503.5 MB (5.48%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
     int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), left = 0, right = queries.size();
+        int l = 0, r = queries.size();
 
-        // Zero array isn't formed after all queries are processed
-        if (!canFormZeroArray(nums, queries, right)) return -1;
+        if(!zeroArray(nums, queries, r)) return -1;
 
-        // Binary Search
-        while (left <= right) {
-            int middle = left + (right - left) / 2;
-            if (canFormZeroArray(nums, queries, middle)) {
-                right = middle - 1;
-            } else {
-                left = middle + 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            if (zeroArray(nums, queries, mid)){
+                r = mid -1;
+            }
+            else{
+                l = mid + 1;
             }
         }
-
-        // Return earliest query that zero array can be formed
-        return left;
+        return l;
     }
 
-private:
-    bool canFormZeroArray(vector<int>& nums, vector<vector<int>>& queries,
-                          int k) {
-        int n = nums.size(), sum = 0;
-        vector<int> differenceArray(n + 1);
-
-        // Process query
-        for (int queryIndex = 0; queryIndex < k; queryIndex++) {
-            int start = queries[queryIndex][0], end = queries[queryIndex][1],
-                val = queries[queryIndex][2];
-
-            // Process start and end of range
-            differenceArray[start] += val;
-            differenceArray[end + 1] -= val;
+    bool zeroArray(const vector<int>& nums, const vector<vector<int>>& queries, int k){
+        const int n = nums.size();
+        vector<int> diffArr(n+1);
+        for(int i = 0; i < k; ++i){
+            vector<int> q = queries[i];
+            int l = q[0], r = q[1], v = q[2];
+            diffArr[l] += v;
+            diffArr[r+1] -= v;
         }
 
-        // Check if zero array can be formed
-        for (int numIndex = 0; numIndex < n; numIndex++) {
-            sum += differenceArray[numIndex];
-            if (sum < nums[numIndex]) return false;
+        int sum = 0;
+        for(int i = 0; i < n; ++i){
+            sum += diffArr[i];
+            if(sum < nums[i]) return false;
         }
         return true;
     }
