e920f7bc65a6122f557064ad517e3a2a34ee5a93
hleesa <salee2@student.42seoul.kr>
2025-03-30 10:21:14 +0900

    Time: 39 ms (69.84%), Space: 87 MB (80.8%) - LeetHub

--- 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> numsArray;
-        int result = 0;
-
-        // Flatten the grid into numsArray
-        for (int row = 0; row < grid.size(); row++) {
-            for (int col = 0; col < grid[0].size(); col++) {
-                numsArray.push_back(grid[row][col]);
+        vector<int> diffs;
+        int base = grid.front().front();
+        for(const auto& rows : grid){
+            for(const auto& val : rows){
+                int diff = val - base;
+                if (diff % x != 0){
+                    return -1;
+                }
+                diffs.push_back(diff/x);
             }
         }
-
-        int length = numsArray.size();
-
-        // Partially sorts the array so that the median element is placed at the
-        // middle index
-        nth_element(numsArray.begin(), numsArray.begin() + length / 2,
-                    numsArray.end());
-
-        // Store the median element as the final common value to make all
-        // elements equal to
-        int finalCommonNumber = numsArray[length / 2];
-
-        for (int number : numsArray) {
-            // If the remainder when divided by x is different for any number,
-            // return -1
-            if (number % x != finalCommonNumber % x) return -1;
-            // Add the number of operations required to make the current number
-            // equal to finalCommonNumber
-            result += abs(finalCommonNumber - number) / x;
+        sort(diffs.begin(), diffs.end());
+        int median = diffs[diffs.size()/2];
+        int minOp = 0;
+        for(auto diff : diffs){
+            minOp += abs(diff - median);
         }
-
-        return result;
+        return minOp;
     }
 };
