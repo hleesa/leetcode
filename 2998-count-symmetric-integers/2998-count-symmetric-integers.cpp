e0bbfc7c7bb5fae0c8bed0300bb5df9ab8d6ed2e
hleesa <salee2@student.42seoul.kr>
2025-04-12 00:11:05 +0900

    Time: 62 ms (74.11%), Space: 9.7 MB (74.11%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     int countSymmetricIntegers(int low, int high) {
         int ret = 0;
-        for(int x = max(low, 10); x <= high; ++x){
-            deque<int> dq;
-            int num = x;
-            while(num){
-                dq.push_back(num%10);
-                num /= 10;
+        for(int x = low; x <= high; ++x){
+            string numStr = to_string(x);
+            const int n = numStr.size();
+            if(n%2 != 0) continue;
+            int sum[2] = {};
+            for(int i = 0; i < n / 2; ++i){
+                sum[0] += numStr[i];
+                sum[1] += numStr[n - 1 - i];
             }
-            if (dq.size() % 2 != 0) continue;
-            const int n = dq.size() / 2;
-            int fSum = accumulate(dq.begin(), dq.begin() + n, 0);
-            int lSum = accumulate(dq.begin() + dq.size() - n,  dq.end(), 0);
-            if (fSum == lSum) {
-                ++ret;
-            }
+            if(sum[0] == sum[1]) ++ret;
         }
         return ret;
     }
