3a19c80921cd064b2d676629893ae58a53059789
hleesa <salee2@student.42seoul.kr>
2026-05-20 19:00:18 +0900

    Time: 0 ms (100%), Space: 8.4 MB (37.83%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isHappy(int n) {
-        long long num = n;
-        set<long long> vis;
+        int num = n;
+        set<int> vis;
         vis.insert(n);
-        for(int i = 0; ; ++i){
-            long long digitSquareSum = 0;
+        while(num != 1){
+            int digitSquareSum = 0;
             while(num != 0){
-                long long digit = num % 10;
+                int digit = num % 10;
                 digitSquareSum += digit * digit;
                 num /= 10;
             }
