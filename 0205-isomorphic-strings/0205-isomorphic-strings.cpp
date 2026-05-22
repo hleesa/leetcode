3a715704258f9160c41d2c601af14ab154fc0924
hleesa <salee2@student.42seoul.kr>
2026-05-22 18:19:27 +0900

    Time: 0 ms (100%), Space: 9.2 MB (80.75%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,9 +3,11 @@ public:
     bool isIsomorphic(string s, string t) {
         vector<int> stot(255, -1), ttos(255,-1);
         for(size_t i = 0 ; i < s.length(); ++i){
-            if(stot[s[i]] == -1) stot[s[i]] = t[i];
-            if(ttos[t[i]] == -1) ttos[t[i]] = s[i];
-            if(stot[s[i]] != t[i] || ttos[t[i]] != s[i]) return false;
+            int si = static_cast<unsigned char>(s[i]);
+            int ti = static_cast<unsigned char>(t[i]);
+            if(stot[si] == -1) stot[si] = ti;
+            if(ttos[ti] == -1) ttos[ti] = si;
+            if(stot[si] != ti || ttos[ti] != si) return false;
         }
 
         return true;
