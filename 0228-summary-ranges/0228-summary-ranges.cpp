75f6ac43d72a0424ec059a41f3bfa113ce0a2e7a
hleesa <salee2@student.42seoul.kr>
2026-06-07 15:34:32 +0900

    Time: 0 ms (100%), Space: 9.3 MB (9.8%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> ans;
+        for(size_t i = 0 ; i < nums.size();){
+            long long off = 1;
+            while(i + off < nums.size() && ((long long)nums[i+off] - (long long)nums[i]) == off){
+                ++off;
+            }
+
+            string range;
+            if(off == 1){
+                range = to_string(nums[i]);
+            }
+            else{
+                range = to_string(nums[i]) + "->" + to_string(nums[i+off-1]);
+            }
+
+            ans.push_back(range);
+            i += off;
+        }
+        
+        return ans;
+    }
+};
